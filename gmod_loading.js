9a10d1294e1fb3d2a0713f4c00323a46d3ce6de3
Mau <mauvepinktree@gmail.com>
2020-11-27 20:40:45 -0300

    Commit 1

---
+++ gmod_loading.js
@@ -0,0 +1,38 @@
+
+var _backgroundimage = "https://i.imgur.com/pHI9YL8.jpg";
+var _servername = "Voltstar Cinema";
+var _mapname = "Lodaing...";
+var _steamid = "";
+
+function Update() {
+	var str = "url(" + _backgroundimage + ")";
+	document.body.style.backgroundImage = str;
+	document.getElementById("title").innerHTML = _servername;
+	document.getElementById("map").innerHTML = _mapname;
+}
+
+function init() {
+	Update();
+}
+
+function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode, volume, language) {
+	_mapname = mapname;
+	_steamid = steamid;
+	Update();
+}
+
+function SetFilesTotal(total) {
+
+}
+
+function DownloadingFile(fileName) {
+
+}
+
+function SetStatusChanged(status) {
+
+}
+
+function SetFilesNeeded(needed) {
+
+}
